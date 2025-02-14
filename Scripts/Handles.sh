ef9ef2ee632ad15d9765226f31bc12437f9e574b
tinkyo <tinkyo@qq.com>
2025-02-14 18:13:41 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -22,6 +22,36 @@ if [ -d *"homeproxy"* ]; then
 	cd $PKG_PATH && echo "homeproxy date has been updated!"
 fi
 
+#预置OpenClash数据
+if [ -d *"openclash"* ]; then
+	mkdir -p files/etc/openclash/core
+	
+	CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-${1}.tar.gz"
+	CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep $1 | awk -F '"' '{print $4}' | grep -v 'v3')
+	CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
+	GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+	GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+	
+	wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
+	wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
+	wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+	wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+	wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+	
+	chmod +x files/etc/openclash/core/clash*
+fi
+
+#预置AdGuardHome数据
+if [ -d *"luci-app-adguardhome"* ]; then
+	mkdir -p files/usr/bin/AdGuardHome
+	
+	AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
+	
+	wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
+	
+	chmod +x files/usr/bin/AdGuardHome/AdGuardHome
+fi
+
 #修改argon主题字体和颜色
 if [ -d *"luci-theme-argon"* ]; then
 	cd ./luci-theme-argon/
