f13e5f69766fdb64cf6ae5bb195ac146e1aa3798
Mucj <mouchaojian@eastedu.com>
2025-03-06 22:38:42 +0800

    整理

--- scripts/diy-script.sh
+++ scripts/diy-script.sh
@@ -1,5 +1,5 @@
 # 修改默认IP & 固件名称
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='.*'/hostname='Ooya'/g" package/base-files/files/bin/config_generate
 
 # TTYD 免登录
@@ -8,12 +8,8 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 # 移除要替换的包
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/alist
-rm -rf feeds/luci/applications/luci-app-alist
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/packages/net/ariang
-#rm -rf package/emortal/luci-app-athena-led
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -26,16 +22,9 @@ function git_sparse_clone() {
 }
 
 # Alist & AdGuardHome & WolPlus & AriaNg & 集客无线AC控制器 & Lucky
-git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome
-git_sparse_clone main https://github.com/kenzok8/small-package tailscale luci-app-tailscale
-git_sparse_clone main https://github.com/kenzok8/small-package nikki luci-app-nikki
-git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
-git_sparse_clone master https://github.com/immortalwrt/packages net/ariang
 git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
-#git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
-#chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
