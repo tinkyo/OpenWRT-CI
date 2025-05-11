f0287c037e0f6226511b56912df26b0c8b657a42
tinkyo <tinkyo@qq.com>
2025-05-11 19:17:09 +0800

    Update diy-script.sh

--- scripts/diy-script.sh
+++ scripts/diy-script.sh
@@ -35,5 +35,8 @@ git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
 # MosDNS
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
+# Alist
+git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
